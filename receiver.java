8838f21b177ab9d3d7a40bba8c60ffe639e5e5b1
tjrob19 <117217062+tjrob19@users.noreply.github.com>
2023-07-27 18:06:22 -0400

    Create receiver.java

---
+++ receiver.java
@@ -0,0 +1,15 @@
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class receiver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String port = args[0];
+		
+		
+	}
+
+}

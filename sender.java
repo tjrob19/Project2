743fc7062b237e5cd701c49545b6ba1e839d81e7
tjrob19 <117217062+tjrob19@users.noreply.github.com>
2023-07-27 18:06:00 -0400

    Create sender.java

---
+++ sender.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class sender {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String port = args[0];
+		String rcvHost = args[1];
+		String rcvPort = args[2];
+		String networkHost = args[3];
+		String networkPort = args[4];
+		
+		
+	}
+
+}

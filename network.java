54e475347a167d3b270606f7f5a6533eb4663408
tjrob19 <117217062+tjrob19@users.noreply.github.com>
2023-07-27 18:06:42 -0400

    Create network.java

---
+++ network.java
@@ -0,0 +1,18 @@
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class network {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String port = args[0];
+		String lostPercent = args[1];
+		String delayedPercent = args[2];
+		String errorPercent = args[3];
+		
+		
+	}
+
+}
